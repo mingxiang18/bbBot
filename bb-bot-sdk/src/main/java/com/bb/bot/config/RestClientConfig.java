8dd9e96868826fbb127b8ba61174f07b7dfe61f8
Ren yuming <1105048721@qq.com>
2024-04-30 16:44:23 +0800

    删除RestClient一些不必要的配置

--- bb-bot-sdk/src/main/java/com/bb/bot/config/RestClientConfig.java
+++ bb-bot-sdk/src/main/java/com/bb/bot/config/RestClientConfig.java
@@ -4,23 +4,14 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
-import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
 import org.apache.hc.client5.http.impl.routing.DefaultProxyRoutePlanner;
-import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactory;
 import org.apache.hc.core5.http.HttpHost;
-import org.apache.hc.core5.ssl.SSLContexts;
-import org.apache.hc.core5.ssl.TrustStrategy;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
-
-import javax.net.ssl.SSLContext;
-import java.security.KeyManagementException;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
 
 /**
  *@author Ren yuming
@@ -50,28 +41,19 @@ public class RestClientConfig {
     }
 
     private ClientHttpRequestFactory getClientHttpRequestFactory() {
-        TrustStrategy trustStrategy = (x509Certificates, s) -> true;
-        try {
-            SSLContext sslContext = SSLContexts.custom().loadTrustMaterial(null,trustStrategy).build();
-            SSLConnectionSocketFactory socketFactory = new SSLConnectionSocketFactory(sslContext);
+        HttpClientBuilder httpClientBuilder = HttpClients.custom();
 
-            HttpClientBuilder httpClientBuilder = HttpClients.custom().setConnectionManager(PoolingHttpClientConnectionManagerBuilder
-                    .create().setSSLSocketFactory(socketFactory).build());
-
-            //如果代理不为空，设置代理
-            if (StringUtils.isNoneBlank(proxyIp) && proxyPort != null) {
-                HttpHost proxy = new HttpHost(proxyIp, proxyPort);
-                DefaultProxyRoutePlanner routePlanner = new DefaultProxyRoutePlanner(proxy);
-                httpClientBuilder.setRoutePlanner(routePlanner);
-            }
-            CloseableHttpClient httpClient = httpClientBuilder.build();
-            HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
-            requestFactory.setHttpClient(httpClient);
-            requestFactory.setConnectionRequestTimeout(readTimeout);
-            requestFactory.setConnectTimeout(connectTimeout);
-            return requestFactory;
-        } catch (NoSuchAlgorithmException | KeyManagementException | KeyStoreException e) {
-            throw new RuntimeException(e);
+        //如果代理不为空，设置代理
+        if (StringUtils.isNoneBlank(proxyIp) && proxyPort != null) {
+            HttpHost proxy = new HttpHost(proxyIp, proxyPort);
+            DefaultProxyRoutePlanner routePlanner = new DefaultProxyRoutePlanner(proxy);
+            httpClientBuilder.setRoutePlanner(routePlanner);
         }
+        CloseableHttpClient httpClient = httpClientBuilder.build();
+        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+        requestFactory.setHttpClient(httpClient);
+        requestFactory.setConnectionRequestTimeout(readTimeout);
+        requestFactory.setConnectTimeout(connectTimeout);
+        return requestFactory;
     }
 }
