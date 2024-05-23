c02c2d4d9ba7fcd274002eb1483ed4878a4ee1cf
Ren yuming <1105048721@qq.com>
2024-05-23 09:40:02 +0800

    完善chatGPT的性格和调用

--- bb-bot-server/src/main/java/com/bb/bot/common/util/aiChat/AiChatClient.java
+++ bb-bot-server/src/main/java/com/bb/bot/common/util/aiChat/AiChatClient.java
@@ -3,6 +3,7 @@ package com.bb.bot.common.util.aiChat;
 import com.alibaba.fastjson2.JSONObject;
 import com.bb.bot.database.chatHistory.entity.ChatHistory;
 import com.bb.bot.util.RestUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -18,6 +19,7 @@ import java.util.List;
  * ai聊天工具类
  * @author ren
  */
+@Slf4j
 @Component
 public class AiChatClient {
 
@@ -45,8 +47,14 @@ public class AiChatClient {
     /**
      * chatGPT的性格
      */
-    @Value("${chatGPT.personality:你的名字是冥想bb。你在一个群聊中，里面有零碎的各种消息，请尝试判断上下文，你的回复要像平时聊天一样。同时你是个鱿鱼偶像，请用最能表现偶像的活力和可爱的方式回复，回复时要加上颜文字。}")
+    @Value("${chatGPT.personality:你的名字是冥想bb，你是一个专业的解答助手。你在一个群聊中，里面有零碎的各种消息。请尝试判断上下文，如果有人提出问题，请从专业的角度进行解答，如果没有，请用自然聊天的方式进行回复。同时你是个鱿鱼偶像，请用最能表现偶像的活力和可爱的方式回复，回复时要加上颜文字。}")
     private String chatGPTPersonality;
+
+    /**
+     * 出现错误时重试次数
+     */
+    @Value("${chatGPT.retryNum:10}")
+    private Integer retryNum;
 
     /**
      * 询问chatGPT
@@ -55,6 +63,17 @@ public class AiChatClient {
      * @author ren
      */
     public String askChatGPT(String question, List<ChatHistory> chatHistoryList) {
+        return askChatGPT(chatGPTPersonality, question, chatHistoryList);
+    }
+
+    /**
+     * 询问chatGPT
+     * @Param personality 机器人设定
+     * @Param question 问题
+     * @Param chatHistoryList 聊天历史
+     * @author ren
+     */
+    public String askChatGPT(String personality, String question, List<ChatHistory> chatHistoryList) {
         //如果apiKey为空，不执行
         if (StringUtils.isBlank(chatGPTApiKey)) {
             return null;
@@ -67,7 +86,7 @@ public class AiChatClient {
         List<ChatGPTContent> chatGPTContentList = new ArrayList<>();
 
         //构建机器人性格消息体
-        chatGPTContentList.add(new ChatGPTContent(ChatGPTContent.SYSTEM_ROLE, chatGPTPersonality));
+        chatGPTContentList.add(new ChatGPTContent(ChatGPTContent.SYSTEM_ROLE, personality));
 
         //如果聊天历史记录不为空，将历史记录构建成消息体
         if (!CollectionUtils.isEmpty(chatHistoryList)) {
@@ -85,9 +104,19 @@ public class AiChatClient {
         //构建提问消息
         chatGPTContentList.add(new ChatGPTContent(ChatGPTContent.USER_ROLE, question));
 
-        //发送请求
-        JSONObject chatGPTResponse = restUtils.post(chatGPTUrl, httpHeaders, new ChatGPTRequest(model, chatGPTContentList), JSONObject.class);
-        //返回chatGPT回复
-        return chatGPTResponse.getJSONArray("choices").getJSONObject(0).getJSONObject("message").getString("content");
+        int nowRetryNum = retryNum;
+        while (nowRetryNum > 0) {
+            try {
+                //发送请求
+                JSONObject chatGPTResponse = restUtils.post(chatGPTUrl, httpHeaders, new ChatGPTRequest(model, chatGPTContentList), JSONObject.class);
+                //返回chatGPT回复
+                return chatGPTResponse.getJSONArray("choices").getJSONObject(0).getJSONObject("message").getString("content");
+            } catch (Exception e) {
+                log.error("chatGPT请求失败，重试次数：" + nowRetryNum, e);
+            } finally {
+                nowRetryNum--;
+            }
+        }
+        return "";
     }
 }
